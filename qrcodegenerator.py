3c13ecb722d30b9cc7cf5be0d5a0b49fb72c340b
Rahul Chavan <64516655+rahulc700@users.noreply.github.com>
2020-04-29 11:00:21 +0530

    Add files via upload

---
+++ qrcodegenerator.py
@@ -0,0 +1,6 @@
+import pyqrcode as rahul
+a=rahul.create("kbtcoe.org")
+#png (portable network grapics)
+#svg (scalable network grapics)   mostly used
+#esp (encapsulated postscript)   gives numeric value
+a.png("kbtcoe.png")
